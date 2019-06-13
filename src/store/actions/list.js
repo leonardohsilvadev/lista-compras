19a8263156f1c1c727519d2721db51727b49e4df
Leonardo Silva <kwtroock@gmail.com>
2019-06-13 16:40:05 -0300

    const types,  combineReducer list, switch types

--- src/store/actions/list.js
+++ src/store/actions/list.js
@@ -1,6 +1,10 @@
+export const Types = {
+    ADD_PRODUCT: "ADD_PRODUCT",
+}
+
 export const Creators = {
     addProduct: (product, list) => ({
-            type: "ADD_PRODUCT",
+            type: Types.ADD_PRODUCT,
             product,
             list,
     }),
